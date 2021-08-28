60f155a66163991b8fecdf382407c9fa40ba7ad9
FrancoPersonal <jafr6@hotmail.com>
2021-08-28 15:39:33 -0500

    change folder

---
+++ src/CloudwatchLocalstack/CloudwatchLocalstack/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace CloudwatchLocalstack
+{
+    internal class Program
+    {
+        private static ServiceProvider provider = null;
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Initialize();
+            CreateLogSample();
+            Console.ReadKey();
+        }
+
+        private static void CreateLogSample()
+        {
+            CloudwatchBasic cloudwatchbasic = provider.GetService<CloudwatchBasic>();
+            cloudwatchbasic.CreateLogSample();
+        }
+
+        private static void Initialize()
+        {
+            IServiceCollection services = new ServiceCollection()
+                .InitialServices(true);
+            provider = services.BuildServiceProvider();
+            provider = services.AddSeriloglogger("log_by_logger", provider)
+                .ConfigureServices().BuildServiceProvider();
+        }
+
+
+
+
+
+    }
+}
