6f58adeed3bf3394a40a8a3d61c78cfbf71d3442
FrancoPersonal <32135394+FrancoPersonal@users.noreply.github.com>
2021-08-28 15:27:40 -0500

    initial commit

---
+++ CloudwatchLocalstack/LocalstackBridge.Abstractions/LocalCredentials.cs
@@ -0,0 +1,23 @@
+﻿namespace LocalstackBridge.Abstractions
+{
+    public class LocalCredentials
+    {
+        public LocalCredentials()
+        {
+            AwsAccessKeyId = "111111";
+            AwsSecretAccessKey = "111111";
+            ServiceURL = "http://localhost:4566";
+        }
+
+        public LocalCredentials(string awsAccessKeyId, string awsSecretAccessKey, string serviceURL)
+        {
+            AwsAccessKeyId = awsAccessKeyId;
+            AwsSecretAccessKey = awsSecretAccessKey;
+            ServiceURL = serviceURL;
+        }
+
+        public string AwsAccessKeyId { get; }
+        public string AwsSecretAccessKey { get; }
+        public string ServiceURL { get; }
+    }
+}
