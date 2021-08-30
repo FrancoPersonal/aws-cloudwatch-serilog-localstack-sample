9c9215d5867759ae83a17bd36da5f1e0315e58d9
FrancoPersonal <jafr6@hotmail.com>
2021-08-30 10:39:36 -0500

    add sample

--- src/CloudwatchLocalstack/CloudwatchLocalstack/CloudwatchBasic.cs
+++ src/CloudwatchLocalstack/CloudwatchLocalstack/CloudwatchBasic.cs
@@ -18,35 +18,58 @@ namespace CloudwatchLocalstack
             this.logger = logger;
         }
 
+
+        public void TestLog()
+        {
+            logger.LogInformation("Initial Test");
+
+            for (int i = 0; i < 10; i++)
+            {
+                logger.LogInformation($"process {i}");
+            }
+        }
+
         public void CreateLogSample(string logGroupName)
         {
             try
             {
-                logger.LogInformation("Initial");
+                logger.LogInformation("Initial Processing");
 
                 DescribeLogGroupsResponse results = cloudwatch.DescribeLogGroupsAsync().GetAwaiter().GetResult();
                 System.Collections.Generic.IEnumerable<string> names = results.LogGroups.Select(log => log.LogGroupName);
-                Console.WriteLine(JsonConvert.SerializeObject(names));
+                LogInfo(JsonConvert.SerializeObject(names));
                 if (!names.Any(name => name == logGroupName))
                 {
                     CreateLogGroupResponse response = cloudwatch.CreateLogGroupAsync(new CreateLogGroupRequest(logGroupName: logGroupName)).GetAwaiter().GetResult();
-                    Console.WriteLine(response.HttpStatusCode);
+                    LogInfo($"{response.HttpStatusCode}");
                     logger.LogInformation("response {status}", response.HttpStatusCode);
                 }
-                else
+                logger.LogInformation("finish Processing");
+                LogInfo("finish");
+                if (names.Any(name => name == logGroupName))
                 {
                     FilterLogEventsResponse fields = cloudwatch.FilterLogEventsAsync(new FilterLogEventsRequest { LogGroupName = logGroupName }).GetAwaiter().GetResult();
-
                     fields.Events.ForEach(e => Console.WriteLine(e.Message));
                 }
-                Console.WriteLine("finish");
                 Console.ReadKey();
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);
+                LogError(ex.Message);
                 logger.LogError("response {error}", ex);
             }
         }
+
+        private void LogInfo(string message)
+        {
+            Console.WriteLine(message);
+            logger.LogInformation(message);
+        }
+        private void LogError(string message)
+        {
+            Console.WriteLine(message);
+            logger.LogError(message);
+        }
+
     }
 }
